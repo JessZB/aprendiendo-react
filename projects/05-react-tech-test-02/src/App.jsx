0bedef50db3a946afb3a3dcaa3199b99dcdbb5e2
JessZB <jesusnbz22@gmail.com>
2024-03-04 10:41:17 -0400

    new advances

--- projects/05-react-tech-test-02/src/App.jsx
+++ projects/05-react-tech-test-02/src/App.jsx
@@ -1,18 +1,37 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Shows } from "./api/components/Shows";
 
 import { useShows } from "./useShows";
+import { useSearch } from "./useSearch";
 
 export const App = () => {
   const { shows } = useShows();
+  const { search, setSearch, error } = useSearch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ search });
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="page">
       <h1>BUSCADOR DE ALGO :D</h1>
       <header>
-        <form>
-          <input type="search" name="search" />
+        <form onSubmit={handleSubmit}>
+          <input
+            value={search}
+            onChange={handleChange}
+            type="search"
+            name="query  "
+          />
           <button type="submit">BUSCAR</button>
         </form>
+        <p>{error}</p>
       </header>
       <main className="show-list">
         <Shows shows={shows} />
